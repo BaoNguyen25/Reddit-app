ce6ae91a264ded45d864087261f4dd32d31ba689
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-09-07 16:48:53 +0700

    build activation email

--- src/main/java/com/example/Reddit/service/AuthService.java
+++ src/main/java/com/example/Reddit/service/AuthService.java
@@ -2,13 +2,16 @@ package com.example.Reddit.service;
 
 import com.example.Reddit.dto.RegisterRequest;
 import com.example.Reddit.model.User;
+import com.example.Reddit.model.VerificationToken;
 import com.example.Reddit.repository.UserRepository;
+import com.example.Reddit.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+import java.util.UUID;
 
 import static java.time.Instant.now;
 
@@ -17,6 +20,7 @@ import static java.time.Instant.now;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
     @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
@@ -27,6 +31,17 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 
     public String encodePassword(String password) {
